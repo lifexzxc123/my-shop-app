8ca9ffbf2fec6a3f6ed95b2e192d0703705df745
lifexzxc123 <vaotapok@bk.ru>
2025-01-13 21:41:54 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <main className="main-content">
+        {/* Здесь будет контент твоего магазина */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
