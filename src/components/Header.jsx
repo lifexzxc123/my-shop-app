96345b71c0dfd56e903e1d08a275f2d521df6e31
lifexzxc123 <vaotapok@bk.ru>
2025-01-13 22:40:07 +0300

    Описание изменений

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
+import 'react';
 import './Header.css';
+import HomeIcon from '../assets/home.svg';
+import MenuIcon from '../assets/menu.svg';
+import CartIcon from '../assets/cart.svg';
 
-function Header() {
-  return (
-    <header className="header">
-      <h1 className="header-title">3D platinum</h1>
-    </header>
-  );
-}
 
-export default Header;
+ function Header() {
+   return (
+     <header className="header">
+         <div className="header-left">
+           <h1 className="header-title">3D platinum</h1>
+           <ul className="header-nav">
+             <li><a href="#"><img src={HomeIcon} alt="Домик" /> Главная</a></li>
+             <li><a href="#"><img src={MenuIcon} alt="Меню" /> Каталог</a></li>
+             <li><a href="#"><img src={CartIcon} alt="Корзина" /> Корзина</a></li>
+         </ul>
+        </div>
+     </header>
+   );
+ }
+
+ export default Header;
